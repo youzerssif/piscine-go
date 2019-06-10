5ac6fccc38eba3c9e9394a2104f03f37641c2498
root <root@localhost.localdomain>
2019-06-10 16:17:10 +0200

    myfamily

---
+++ myfamily.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --argjson id "$HERO_ID " '.[] | select( .id==$id ) | .connections.relatives' | sed -e 's/"//g'
